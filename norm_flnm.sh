91df09e40c8923ea3742c0cf7bec9a0b2153d6d7
Oliver Yuan <iyhpi@sina.com>
2019-11-07 09:43:13 +0800

    deleted file test

---
+++ norm_flnm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Replace special characters in a filename with spaces to normalize it.
+# . norm_flnm.sh
+# spec_char='–—"#$%&\+,-/:;<=>?@[].\`|}~^!{)(* '
+spec_char='–—_"#$%&\+,-/:;<=>?@\`|}~^!{)(* '
+function norm_flnm {
+    local path="$1"
+    for ((i=0;i<${#path};i++));do
+        tmp="${path:i:1}"
+        if [[ "$spec_char" =~ "$tmp" ]];then
+            # if special characters in the spec_char set are found in a filename, they shall be substituted with whitespaces
+            path=${path//"$tmp"/ }
+        fi
+    done
+     # an open bracket is substituted with an underline sign, and a closing bracket is deleted
+    path=$(echo $path | sed '{s/ \?\[/_/g;s/\]//g;s/ \+/ /g;s/^_//}')
+    # return the substituted result
+    echo $path
+}
